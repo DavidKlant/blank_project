11b345d16fb5917ff3df01ab6e4e0810b7598a02
David Klant <david@card-klant.de>
2022-09-29 21:57:51 +0200

    Add test remidner

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,4 +1,5 @@
 import 'package:blank_project/functionality/notifications/local_notification_schedule.dart';
+import 'package:blank_project/models/internal/notification_model.dart';
 import 'package:flutter/material.dart';
 
 class WelcomePage extends StatelessWidget {
@@ -20,6 +21,16 @@ class WelcomePage extends StatelessWidget {
             ElevatedButton(
                 onPressed: () {
                   LocalNotificationSchedule.instantNotification();
+                },
+                child: const Text("Instant notification")),
+            ElevatedButton(
+                onPressed: () {
+                  LocalNotificationSchedule.scheduleNotification(
+                      NotificationModel(
+                          id: 1,
+                          body: "body",
+                          scheduledDate: DateTime(2022, 9, 29, 21, 56, 55),
+                          title: "title3"));
                 },
                 child: const Text("Instant notification")),
           ],
